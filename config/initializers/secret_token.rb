5757025ab34c2b97140e76df24f711e42cd9b664
Blake Holmes <bjholmes23@gmail.com>
2013-04-08 14:13:01 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wuxilead::Application.config.secret_token = '21bab7b487bf9c7094034b6541d5609257308264aa61583f42bb5dd6bfd55b014920eaec2e5e3fffb7b665a3ec44aa31ef10e7ecd09076e4b65f6e725fce4632'
